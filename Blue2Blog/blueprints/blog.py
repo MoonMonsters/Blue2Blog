31a432017fa18fab3f3ab96f4458b4196df17c2d
MoonMonsters <qxinhai@yeah.net>
2018-09-13 22:36:20 +0800

    完成评论显示功能

--- Blue2Blog/blueprints/blog.py
+++ Blue2Blog/blueprints/blog.py
@@ -4,7 +4,7 @@
 from flask import Blueprint, current_app
 from flask import render_template, request
 
-from Blue2Blog.models import Post, Category
+from Blue2Blog.models import Post, Category, Comment
 
 blog_bp = Blueprint('blog', __name__)
 
@@ -49,7 +49,23 @@ def show_category(category_id):
 
 @blog_bp.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def show_post(post_id):
+	# 博客内容
 	post = Post.query.get_or_404(post_id)
+
+	page = request.args.get('page', 1, type=int)
+	per_page = current_app.config['BLUE2BLOG_COMMENT_PER_PAGE']
+	pagination = Comment.query.with_parent(post).filter_by(reviewed=True).order_by(Comment.timestamp.asc()).paginate(
+		page, per_page)
+	# 获取文章评论
+	comments = pagination.items
+
 	context = {}
 	context.update(post=post)
+	context.update(comments=comments)
+	context.update(pagination=pagination)
 	return render_template('blog/post.html', **context)
+
+
+@blog_bp.route('/comment/<int:comment_id>', methods=['GET', 'POST'])
+def reply_comment(comment_id):
+	pass
